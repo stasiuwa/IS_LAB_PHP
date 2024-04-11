a6dbe31f0b3ef982d619df24af5e054e8d4caedf
ziemniakEVO2 <pwinski013@gmail.com>
2024-04-11 17:20:58 +0200

    init

---
+++ IS_Lab3_PHP/IS_LAB3_JMETER/index2.php
@@ -0,0 +1,16 @@
+<?php
+// example of update actor table, all first_name ADAM will beupdated to CHRIS
+$servername = "localhost";
+$username = "sakila2";
+$password = "pass";
+$database = "sakila";
+$conn = new mysqli($servername, $username, $password,
+    $database);
+if ($conn->connect_error) die("Database connection failed: " . $conn->connect_error);
+
+ echo "Databse connected successfully, username " . $username . "<br><br>";
+ $sql = "UPDATE actor SET first_name = 'CHRIS' WHERE first_name = 'ADAM'";
+ $conn->query($sql);
+ echo "Table actor updated – all ADAM to CHRIS";
+
+ $conn->close();
